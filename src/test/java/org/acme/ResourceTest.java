48a2d949753e8c76ef53e434f63113faa1401914
Davide Ginnasio <d.ginnasio@tdnet.it>
2021-11-24 11:46:22 +0100

    merge conflict

--- src/test/java/org/acme/ResourceTest.java
+++ src/test/java/org/acme/ResourceTest.java
@@ -3,32 +3,29 @@ package org.acme;
 import io.quarkus.test.junit.QuarkusTest;
 import org.acme.api.ResourceDTO;
 import org.acme.api.ResourceProducer;
-import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
-import java.util.Date;
 import java.util.concurrent.CompletableFuture;
 
 @QuarkusTest
-//@QuarkusTestResource(KafkaResource.class)
 public class ResourceTest {
     @Inject
     ResourceProducer resourceProducer;
 
     @Test
     public void test() {
-        ResourceDTO resourceDTO = new ResourceDTO();
-        resourceDTO.setMessage("message " + new Date());
-        var result1 = resourceProducer.send(resourceDTO);
-        var result2 = resourceProducer.send(resourceDTO);
-        var result3 = resourceProducer.send(resourceDTO);
+        var result1 = resourceProducer.send(new ResourceDTO());
+        var result2 = resourceProducer.send(new ResourceDTO());
+        var result3 = resourceProducer.send(new ResourceDTO());
+        var result4 = resourceProducer.send(new ResourceDTO());
         try {
-            CompletableFuture.allOf(result1, result2, result3);
+            CompletableFuture.allOf(result1, result2, result3, result4);
             // wait for consumer
             Thread.sleep(10000);
         } catch (Exception ex) {
-            Assert.fail(ex.getMessage());
+            Assertions.fail(ex.getMessage());
         }
     }
 
