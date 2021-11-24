230eebc2f3f3fe85eaaec53e0ba08bbbcf91a794
Davide Ginnasio <d.ginnasio@tdnet.it>
2021-11-24 10:07:11 +0100

    multi partition

--- src/main/java/org/acme/api/ResourceConsumer.java
+++ src/main/java/org/acme/api/ResourceConsumer.java
@@ -1,6 +1,7 @@
 package org.acme.api;
 
 import io.smallrye.reactive.messaging.annotations.Blocking;
+import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
@@ -17,23 +18,15 @@ public class ResourceConsumer {
     @Retry(delay = 20, delayUnit = ChronoUnit.SECONDS, maxRetries = 0)
     @Blocking(ordered = false)
     @Acknowledgment(Acknowledgment.Strategy.MANUAL)
-    public CompletionStage<Void> process(Message<ResourceDTO> incomingMessage) {
+    public CompletionStage<Void> process(Message<ResourceDTO> message) {
         try {
-
-//            log.info("AtomicLong:" + wip.incrementAndGet());
-            System.out.println("[concurrent-test]" + Thread.currentThread().getName() + " - start - ");
-
+            var partitionNumber = message.getMetadata().get(OutgoingKafkaRecordMetadata.OutgoingKafkaRecordMetadataBuilder.class).get().build().getPartition();
+            System.out.println("[concurrent-test]" + Thread.currentThread().getName() + " - start - with partition: " + partitionNumber);
             Thread.sleep(2000);
-
-            //processMessage(incomingMessage);
-            System.out.println("[concurrent-test]" + Thread.currentThread().getName() + " - end - ");
-
-
-//            wip.decrementAndGet();
-
-            return incomingMessage.ack();
+            System.out.println("[concurrent-test]" + Thread.currentThread().getName() + " - end - with partition: " + partitionNumber);
+            return message.ack();
         } catch (Exception e) {
-            return incomingMessage.nack(e);
+            return message.nack(e);
         }
     }
 
--- src/main/java/org/acme/api/ResourceProducer.java
+++ src/main/java/org/acme/api/ResourceProducer.java
@@ -1,12 +1,15 @@
 package org.acme.api;
 
 import io.smallrye.reactive.messaging.annotations.Broadcast;
+import io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
-import org.eclipse.microprofile.reactive.messaging.OnOverflow;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Metadata;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 @ApplicationScoped
@@ -17,6 +20,16 @@ public class ResourceProducer {
     Emitter<ResourceDTO> resourceDTOEmitter;
 
     public CompletableFuture<Void> send(ResourceDTO resourceDTO) {
-        return resourceDTOEmitter.send(resourceDTO).toCompletableFuture();
+        Random random = new Random();
+        var metadata = Metadata.of(
+                OutgoingKafkaRecordMetadata.builder()
+                        .withPartition(random.ints(0, 3).findFirst().getAsInt())//number of partiton
+
+        );
+
+        var message = Message.of(resourceDTO, metadata);
+        resourceDTOEmitter.send(message);
+        return CompletableFuture.completedFuture(null);
+        //return resourceDTOEmitter.send(resourceDTO).toCompletableFuture();
     }
 }
