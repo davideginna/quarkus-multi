e83ad0cbf70287bdb20981b303f4ec43165f9f62
Silvio  Giannini <giannini.silvio@gmail.com>
2021-11-24 09:02:41 +0100

    fix test

--- src/test/java/org/acme/ResourceTest.java
+++ src/test/java/org/acme/ResourceTest.java
@@ -3,10 +3,12 @@ package org.acme;
 import io.quarkus.test.junit.QuarkusTest;
 import org.acme.api.ResourceDTO;
 import org.acme.api.ResourceProducer;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import javax.inject.Inject;
 import java.util.Date;
+import java.util.concurrent.CompletableFuture;
 
 @QuarkusTest
 //@QuarkusTestResource(KafkaResource.class)
@@ -18,7 +20,16 @@ public class ResourceTest {
     public void test() {
         ResourceDTO resourceDTO = new ResourceDTO();
         resourceDTO.setMessage("message " + new Date());
-        resourceProducer.send(resourceDTO);
+        var result1 = resourceProducer.send(resourceDTO);
+        var result2 = resourceProducer.send(resourceDTO);
+        var result3 = resourceProducer.send(resourceDTO);
+        try {
+            CompletableFuture.allOf(result1, result2, result3);
+            // wait for consumer
+            Thread.sleep(10000);
+        } catch (Exception ex) {
+            Assert.fail(ex.getMessage());
+        }
     }
 
 }
